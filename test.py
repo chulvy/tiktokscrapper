9a73cc3cbad89746e8a550301ee0cd6a6ae18dc4
vachulyu <vachulyu@mts.ru>
2023-09-13 13:53:03 +0300

    added ms token from TikTok cookie

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import asyncio
 import os
 from datetime import datetime
 
-ms_token = os.environ.get("ms_token", None)  # get your own ms_token from your cookies on tiktok.com
+ms_token = os.environ.get("gErPUgA9_iz5w0BoF_pBBQgYbIr0QL4TgcrTVz6GePp6h2PDU7PRqlpluvbtjue5dBeYA0m_oSBBJ-Y8VWicqQssDVB04LBGGILhySi36628gEIuvAzNf1-mDuyh0SKl9FoUy00O37s=", None)  # get your own ms_token from your cookies on tiktok.com
 
 
 async def trending_videos():
